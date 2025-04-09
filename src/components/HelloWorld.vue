226fb00c7a84f27bfbd7f7b78bc092a5e82f9b50
Gerardo Diaz <gerardo.diaz@sofoscorp.com>
2025-04-09 03:35:19 -0400

    chore: format main vue file

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -23,12 +23,8 @@
           {{ item.genres.join(', ') }}
         </template>
         <template v-slot:item.actions="{ item }">
-          <v-icon class="mr-2" @click="openEditDialog(item)">
-            mdi-pencil
-          </v-icon>
-          <v-icon @click="deleteBook(item)">
-            mdi-delete
-          </v-icon>
+          <v-icon class="mr-2" @click="openEditDialog(item)"> mdi-pencil </v-icon>
+          <v-icon @click="deleteBook(item)"> mdi-delete </v-icon>
         </template>
       </v-data-table>
 
@@ -53,11 +49,11 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue'
-import 'firebase/firestore'
+import { ref, onMounted } from 'vue';
+import 'firebase/firestore';
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc, updateDoc } from 'firebase/firestore/lite';
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -79,86 +75,107 @@ const db = getFirestore(app);
 
 export default {
   setup() {
-    const bookToAdd = ref({ name: '', author: '', year: '', genres: [] })
-    const genres = ref(['Ficción', 'No ficción', 'Ciencia ficción', 'Fantasía', 'Misterio', 'Biografía', 'Terror', 'Romance'])
-    const books = ref([])
+    const bookToAdd = ref({ name: '', author: '', year: '', genres: [] });
+    const genres = ref([
+      'Ficción',
+      'No ficción',
+      'Ciencia ficción',
+      'Fantasía',
+      'Misterio',
+      'Biografía',
+      'Terror',
+      'Romance',
+    ]);
+    const books = ref([]);
     const headers = ref([
       { title: 'Nombre', key: 'name' },
       { title: 'Autor', key: 'author' },
       { title: 'Año', key: 'year', sortable: true },
       { title: 'Género', key: 'genres' },
-      { title: 'Acciones', key: 'actions' }
-    ])
+      { title: 'Acciones', key: 'actions' },
+    ]);
 
-    const isEditDialogOpen = ref(false)
-    const bookToEdit = ref({ name: '', author: '', year: '', genres: [] })
+    const isEditDialogOpen = ref(false);
+    const bookToEdit = ref({ name: '', author: '', year: '', genres: [] });
 
     const addBook = async () => {
       try {
-        const docRef = await addDoc(collection(db, "books"), bookToAdd.value);
+        const docRef = await addDoc(collection(db, 'books'), bookToAdd.value);
         const newBook = {
-          'id': docRef.id,
-          ...bookToAdd.value
-        }
+          id: docRef.id,
+          ...bookToAdd.value,
+        };
         books.value.push(newBook);
         bookToAdd.value = { name: '', author: '', year: '', genres: [] };
       } catch (e) {
-        console.error("Error adding document: ", e);
+        console.error('Error adding document: ', e);
       }
-    }
+    };
 
     const deleteBook = async (item) => {
-      const deletedBook = await deleteDoc(doc(db, "books", item.id));
-      books.value  = books.value.filter(b => b.id != item.id);
-    }
+      const deletedBook = await deleteDoc(doc(db, 'books', item.id));
+      books.value = books.value.filter((b) => b.id != item.id);
+    };
 
     // Open edit dialog with the selected book to edit
     const openEditDialog = (item) => {
       bookToEdit.value = item;
       isEditDialogOpen.value = true;
-    }
+    };
 
     // Close edit dialog and reset bookToEdit
     const closeEditDialog = () => {
       bookToEdit.value = { name: '', author: '', year: '', genres: [] };
       isEditDialogOpen.value = false;
-    }
+    };
 
     const saveEdit = async () => {
       try {
         // Get a reference to the book document
-        const bookRef = doc(db, "books", bookToEdit.value.id);
+        const bookRef = doc(db, 'books', bookToEdit.value.id);
 
         // Update the book in Firestore
         await updateDoc(bookRef, bookToEdit.value);
 
         // Update local books
-        const bookIndex = books.value.findIndex(book => book.id === bookToEdit.value.id);
+        const bookIndex = books.value.findIndex((book) => book.id === bookToEdit.value.id);
         books.value[bookIndex] = { ...bookToEdit.value };
 
         // Close dialog
         closeEditDialog();
       } catch (e) {
-        console.error("Error updating document: ", e);
+        console.error('Error updating document: ', e);
       }
-    }
+    };
 
     onMounted(async () => {
       const booksCol = collection(db, 'books');
       const booksSnapshot = await getDocs(booksCol);
-      const booksList = booksSnapshot.docs.map(doc => ({
-        'id': doc.id, 
-        ...doc.data()
+      const booksList = booksSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
       }));
 
       console.log(booksList);
 
-      booksList.forEach(book => {
-        books.value.push(book)
-      })
-    })
+      booksList.forEach((book) => {
+        books.value.push(book);
+      });
+    });
 
-    return { bookToAdd, genres, books, headers, addBook, deleteBook, isEditDialogOpen, bookToEdit, openEditDialog, closeEditDialog, saveEdit }
-  }
-}
+    return {
+      bookToAdd,
+      genres,
+      books,
+      headers,
+      addBook,
+      deleteBook,
+      isEditDialogOpen,
+      bookToEdit,
+      openEditDialog,
+      closeEditDialog,
+      saveEdit,
+    };
+  },
+};
 </script>
